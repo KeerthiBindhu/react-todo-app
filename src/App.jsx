c3d5410adf24e16dc891ab5e6a6a3aa1b8febc30
Keerthi Bindu <binubalachander@gmail.com>
2026-07-10 22:10:49 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { TodoForm } from "./components/TodoForm/TodoForm";
+import { TodoList } from "./components/TodoList/TodoList";
+import { TodoFilters } from "./components/TodoFilters/TodoFilters";
+import { Alert } from "./components/Alert/Alert";
+import { Loader } from "./components/Loader/Loader";
+import { useTodos } from "./hooks/todo";
+import todoImage from './assets/to-do.png';
+import styles from "./App.module.css";
+
+function App() {
+  const todos = useTodos();
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src={todoImage} />
+        <h2 className={styles.Title}>To-Do App</h2>
+      </header>
+
+      <div className={styles.AppContainer}>
+        {todos.isLoading && <Loader />}
+        {!!todos.error.message && (
+          <Alert onClear={todos.error.clear}>{todos.error.message}</Alert>
+        )}
+        <TodoForm onCreate={todos.create} />
+        <TodoFilters onFilter={todos.filter} />
+        <TodoList
+          todos={todos.data}
+          onUpdate={todos.update}
+          onDelete={todos.delete}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
